4f681cbe76d254de6b60178ac10b5999fd60dfbe
sunnyjavamoon <sunnyjavamoon@gmail.com>
2012-04-17 06:31:48 +0000

    listview

--- look-beautiful/src/robot/arm/ArtActivity.java
+++ look-beautiful/src/robot/arm/ArtActivity.java
@@ -3,8 +3,13 @@
  */
 package robot.arm;
 
+import java.util.Arrays;
+import java.util.List;
+
 import robot.arm.common.BaseActivity;
+import robot.arm.utils.BaseUtils;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * @author li.li
@@ -13,11 +18,19 @@ import android.os.Bundle;
  * 
  */
 public class ArtActivity extends BaseActivity {
+	private static List<Integer> list = Arrays.asList(R.layout.images_content_row, R.layout.images_content_row, R.layout.images_content_row,
+			R.layout.images_content_row);
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.art_content);
+
+		ListView lv = (ListView) findViewById(R.id.images);
+		ImagesAdapter ia = new ImagesAdapter(this, list);
+		lv.setAdapter(ia);
+		BaseUtils.setListViewHeight(lv);// 设置listview真实高度
 	}
 
 	@Override
