4e40599a44bb1692e46d371427c6de854408a424
liuy.dev <liuy.dev@gmail.com>
2012-05-08 15:30:29 +0000

    bugfix

--- mokoClient/src/com/mokoclient/core/bean/PostBean.java
+++ mokoClient/src/com/mokoclient/core/bean/PostBean.java
@@ -2,23 +2,11 @@ package com.mokoclient.core.bean;
 
 public class PostBean {
 
-	private String vocationName;
-
-	public String getVocationName() {
-		return vocationName;
-	}
-
-	public void setVocationName(String vocationName) {
-		this.vocationName = vocationName;
-	}
-
 	private String title;
 	private String coverUrl;
 	private String detailUrl;
 
-	public PostBean(String vocationName, String title, String coverUrl,
-			String detailUrl) {
-		this.vocationName = vocationName;
+	public PostBean(String title, String coverUrl, String detailUrl) {
 		this.title = title;
 		this.coverUrl = coverUrl;
 		this.detailUrl = detailUrl;
