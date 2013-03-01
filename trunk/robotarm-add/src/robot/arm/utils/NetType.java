20ed464c3d0d27c7527d4b0e5575d591001f501d
sunnyjavamoon@gmail.com <sunnyjavamoon@gmail.com@91ceb67d-a15c-3b18-367b-d19b16655ab7>
2013-03-01 08:55:50 +0000

    重写了部分组件 1. SD卡缓存组件 2. 异步任务组件 3. 图片缓存组件 4. 网络检测组件 5. HTTP请求组件

---
+++ trunk/robotarm-add/src/robot/arm/utils/NetType.java
@@ -0,0 +1,32 @@
+package robot.arm.utils;
+
+/**
+ * 当前网络类型
+ * 
+ * @author li.li
+ * 
+ *         Jul 26, 2012
+ */
+public enum NetType {
+	TYPE_WIFI("WIFI网络"), //
+	TYPE_2G("2G手机网络"), //
+	TYPE_3G_OR_OTHERS("3G或其它手机网络"), //
+	TYPE_UNKNOWN("未知网络"), //
+	TYPE_NONE("无可用网络"), //
+	;
+
+	private String desc;// 网络连接描述
+
+	private NetType(String desc) {
+		this.desc = desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+}
